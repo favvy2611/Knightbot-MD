f9cdbb058ed39ce1e929ae074321c29b5b032dd3
favvy2611 <favvyemma9@gmail.com>
2025-05-30 10:16:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const store = {
     }
 }
 
-let phoneNumber = "911234567890"
+let phoneNumber = "2348139598985"
 let owner = JSON.parse(fs.readFileSync('./data/owner.json'))
 
 global.botname = "KNIGHT BOT"
