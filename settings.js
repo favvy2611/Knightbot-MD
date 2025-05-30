be06db22d71151121da649d9c716cc5d2aabf049
favvy2611 <favvyemma9@gmail.com>
2025-05-30 10:32:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '2348123587197', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Him', // Your name
+  ownerNumber: '2348139598985', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.1",
 };
